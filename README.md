0ef0a4a46fa5af2f1dad0b24c654a1aa8f4272e5
Susanna <thesuss@users.noreply.github.com>
2016-10-26 14:54:31 +0200

    Scaffolded rails application  (#1)
    * set up rails scaffold
    
    * initialized rspec
    
    * initialized cucumber
    
    * added things to gitignore
    
    * removed IMMI-projekt and added shf-project and tidied gemfile
    
    * bundled and made sure rake was green
    
    * edited readme
    
    * cleaned up rails helper
    
    * cleaned up spec helper

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+This README is a separate PR
---
+++ config/application.rb
@@ -0,0 +1,35 @@
+require_relative 'boot'
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module SHFProject
+  class Application < Rails::Application
+
+    # Disable generation of helpers, javascripts, css, and view, helper, routing and controller specs
+config.generators do |generate|
+  generate.helper false
+  generate.assets false
+  generate.view_specs false
+  generate.helper_specs false
+  generate.routing_specs false
+  generate.controller_specs false
+end
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
