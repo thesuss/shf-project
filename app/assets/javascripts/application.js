a1f8bf8add5ffd643afc9f097dccc3c7e5a0d311
thesuss <lullisuss@gmail.com>
2016-11-03 12:48:54 +0100

    added js for and background to mobile menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,19 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+jQuery(document).ready(function() {
+    "use strict";
+
+    // Slide mobile navigation from left
+    jQuery('#site-navigation .menu-toggle').on('click', function() {
+        jQuery(this).toggleClass('active');
+        if ( jQuery(this).hasClass('active') ) {
+            jQuery('#site-navigation .menu').animate({left:0}, {duration: 225, easing: 'swing'});
+            return false;
+        } else {
+            jQuery('#site-navigation .menu').animate({left:-291}, {duration: 225, easing: 'linear'});
+            return false;
+        }
+    });
+});
