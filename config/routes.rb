f8b4094695c6caf29cbe7ee55587d8e4ffaab963
thesuss <lullisuss@gmail.com>
2016-11-19 12:08:23 +0100

    adds controller and routes for admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'application#index.html'
   resources :membership_applications, only: [:new, :create, :edit, :update, :index, :show]
-
+  resources :admin, only: [:index]
+  namespace 'admin' do
+    get 'list-applications', to: 'admin#show_membership_applications'
+  end
 end
