d97814691c43a4ed6c2845ef7d145b54c90b0920
thesuss <lullisuss@gmail.com>
2016-11-29 08:20:00 +0700

    added missing stepdefenition for choosing category

--- features/step_definitions/business_category_steps.rb
+++ features/step_definitions/business_category_steps.rb
@@ -9,3 +9,7 @@ And(/^I navigate to the business category edit page for "([^"]*)"$/) do |name|
   business_category = BusinessCategory.find_by(name: name)
   visit edit_business_category_path(business_category)
 end
+
+And(/^I select "([^"]*)" Category/) do |element|
+  page.check(element)
+end
