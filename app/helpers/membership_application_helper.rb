87ea083853622587f8793148c794080658b84f1f
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-28 22:39:27 -0800

    helper to display the first and last names with a space inbetween

--- app/helpers/membership_application_helper.rb
+++ app/helpers/membership_application_helper.rb
@@ -4,4 +4,8 @@ module MembershipApplicationHelper
     policy(@membership_application).permitted_attributes_for_edit.include? :status
   end
 
+  def member_full_name
+    @membership_application ? "#{@membership_application.first_name} #{@membership_application.last_name}" : '..'
+
+  end
 end
