308d88b2eda8c0f8d53f9c95f1a91843f00fb4a9
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-11-30 03:32:51 -0800

    merged business category policy

--- app/policies/business_category_policy.rb
+++ app/policies/business_category_policy.rb
@@ -12,4 +12,29 @@ class BusinessCategoryPolicy < ApplicationPolicy
     true
   end
 
+
+  def index?
+    is_admin?
+  end
+
+
+  def update?
+    is_admin?
+  end
+
+
+  def edit?
+    is_admin?
+  end
+
+
+  def destroy?
+    is_admin?
+  end
+
+
+  private
+  def is_admin?
+    @user.admin? if @user
+  end
 end
