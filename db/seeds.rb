f27c13c075dca126da5545b4753889e939c88c16
Susanna <thesuss@users.noreply.github.com>
2016-12-01 21:17:09 +0700

    Seed users (#41)
    * added user cs
    * simplified csv, added code in seeds to add them as users
    * added fake emails

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(email: 'admin@sverigeshundforetagare.se', password: 'hundapor', admin: true)
+require 'csv'
 
-business_categories = ['Träning',
-                       'Psykologi',
-                       'Rehab', 'Butik', 'Trim', 'Friskvård', 'Dagis', 'Pensionat',
-                       'Skola']
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'user_table.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  User.create(email: row['email'], password: 'whatever')
+  puts row['email'] + ' saved'
+end
+
+business_categories = %w(Träning Psykologi Rehab Butik Trim Friskvård Dagis Pensionat Skola)
 business_categories.each { |b_category| BusinessCategory.create(name: b_category)}
 BusinessCategory.create(name: 'Sociala tjänstehundar', description: 'Terapi-, vård- & skolhund dvs hundar som jobbar tillsammans med sin förare/ägare inom vård, skola och omsorg.')
 BusinessCategory.create(name: 'Civila tjänstehundar', description: 'Assistanshundar dvs hundar som jobbar åt sin ägare som service-, signal, diabetes, PH-hund mm')
