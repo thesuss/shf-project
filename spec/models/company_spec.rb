b91d369bd26f83d312478ad798341e7eee023bba
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-12-01 01:58:21 -0800

    correct copy paste error (should be 'email')

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -25,8 +25,8 @@ RSpec.describe Company, type: :model do
     it { is_expected.to validate_presence_of :company_number }
     it { is_expected.to validate_length_of(:company_number).is_equal_to(10) }
 
-    it { is_expected.to allow_value('user@example.com').for(:contact_email) }
-    it { is_expected.not_to allow_value('userexample.com').for(:contact_email) }
+    it { is_expected.to allow_value('user@example.com').for(:email) }
+    it { is_expected.not_to allow_value('userexample.com').for(:email) }
 
   end
 
