4c5de4a0f68cdc0ee8a6ca75d79ab4993a2696c9
Ashley Engelund <weedySeaDragon@users.noreply.github.com>
2016-11-30 16:33:47 -0800

    Flash - show multiple messages (#39)
    * flash_message(type, text) helper: append flash messages; spec for appending flash messages
    (cherry picked from commit 2af4a0a)
    * move flash messages into a partial
    (cherry picked from commit 505caaf)
    * show the flash message; might be just a String, or might be a collection that needs to be separated by a /br
    (cherry picked from commit 55d5dee)
    * Show a flash message by using helper method. This can handle single or mutiple message for each flash key
    (cherry picked from commit 44a89ca)
    * can add a flash message the normal way, then later add with flash_message(); clean up spec tests
    * add tests/expectations for flash_render_message
    * end the file with a newline

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,21 @@ module ApplicationHelper
       when :alert then 'danger'
     end
   end
+
+  def flash_message(type, text)
+    flash[type] ||= []
+    if flash[type].instance_of? String
+      flash[type] = [flash[type]]
+    end
+    flash[type] << text
+  end
+
+  def render_flash_message(flash_value)
+    if flash_value.instance_of? String
+      flash_value
+    else
+      safe_join(flash_value, '<br/>'.html_safe)
+    end
+  end
+
 end
--- spec/helpers/helper_spec.rb
+++ spec/helpers/helper_spec.rb
@@ -11,4 +11,53 @@ RSpec.describe ApplicationHelper, type: :helper do
       expect(helper.flash_class(:alert)).to eq 'danger'
     end
   end
+
+  describe '#flash_message and #render_flash_message' do
+
+    before(:each) do
+      @flash_type = :blorf
+      @first_message = 'first_message'
+      @second_message = 'second_message'
+      flash[@flash_type] = nil
+      helper.flash_message @flash_type, @first_message
+    end
+
+    describe 'adds message to nil flash[type]' do
+      it { expect(flash[@flash_type].count).to eq 1 }
+      it { expect(flash[@flash_type].first).to eq @first_message }
+      it { expect(helper.render_flash_message(flash[@flash_type])).to eq @first_message }
+    end
+
+
+    describe 'adds message to a flash[type] that already has messages' do
+
+      before (:each) do
+        helper.flash_message @flash_type, @second_message
+      end
+
+      it { expect(flash[@flash_type].count).to eq 2 }
+      it { expect(flash[@flash_type].first).to eq @first_message }
+      it { expect(flash[@flash_type].last).to eq @second_message }
+      it { expect(flash[@flash_type]).to eq [@first_message, @second_message] }
+      it { expect(helper.render_flash_message(flash[@flash_type])).to eq(safe_join([@first_message, @second_message], '<br/>'.html_safe)) }
+    end
+
+
+    describe 'can add a message the default way, then add another with flash_message' do
+
+      before(:each) do
+        @f2_type = :florb
+        flash[@f2_type] = nil
+        flash[@f2_type] = @first_message
+        helper.flash_message @f2_type, @second_message
+      end
+
+      it { expect(flash[@f2_type].count).to eq 2 }
+      it { expect(flash[@f2_type].first).to eq @first_message }
+      it { expect(flash[@f2_type].last).to eq @second_message }
+      it { expect(flash[@f2_type]).to eq [@first_message, @second_message] }
+      it { expect(helper.render_flash_message(flash[@f2_type])).to eq(safe_join([@first_message, @second_message], '<br/>'.html_safe)) }
+    end
+
+  end
 end
