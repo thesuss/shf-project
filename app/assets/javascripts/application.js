499311fcbcd217cf91055f34e3cc01778add72f1
Susanna <thesuss@users.noreply.github.com>
2016-12-04 00:34:53 +0700

    Add relevant navigation and make it work (refactor later) (#50)
    * Mobile submenus do not work
    
    * fixed hash attributes in top partial
    
    * classes before ids
    
    * fixed hash attributes in navigation
    
    * classes before ids gain
    
    * removed js for widgets
    
    * put the social links in a separate partial (ugly whitespace there needs to be fixed)
    
    * Files should end with a trailing newline
    
    * mobile navigation works, regular drop-downs do not
    
    * removed extra file and fixed logo position
    
    * YAY - Added back the classes in the end, and the css for headlines, and now it works for visitor
    
    * ul needs to have .menu
    
    * removed # in menu
    
    * adds membership pages to drop-down
    
    * changed show_company_path to company_path
    
    * changed colour of checked checkboxes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,20 +16,72 @@
 //= require turbolinks
 //= require_tree .
 
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
     "use strict";
 
     // Slide mobile navigation from left
-    jQuery('#site-navigation .menu-toggle').on('click', function() {
+    jQuery('#site-navigation .menu-toggle').on('click', function () {
         jQuery(this).toggleClass('active');
-        if ( jQuery(this).hasClass('active') ) {
-            jQuery('#site-navigation .menu').animate({left:0}, {duration: 225, easing: 'swing'});
+        if (jQuery(this).hasClass('active')) {
+            jQuery('#site-navigation .menu').animate({left: 0}, {
+                duration: 225,
+                easing: 'swing'
+            });
             return false;
         } else {
-            jQuery('#site-navigation .menu').animate({left:-291}, {duration: 225, easing: 'linear'});
+            jQuery('#site-navigation .menu').animate({left: -291}, {
+                duration: 225,
+                easing: 'linear'
+            });
             return false;
         }
     });
+
+
+    /*------------------
+     Main navigation
+     ------------------*/
+    // Add toggle button for mobile navigation sub menus
+    jQuery('.menu-item-has-children > a').after('<span class="toggle-sub-menu"></span>');
+
+    // Mobile nav sub menu toggle button click
+    jQuery('#site-navigation .toggle-sub-menu').on('click', function () {
+        jQuery(this).toggleClass('toggle-sub-menu-active');
+        jQuery(this).next().slideToggle('fast');
+    });
+
+    // Add mobile class to menu on load or resize
+    if (jQuery('body').width() <= 1200) {
+        jQuery('#site-navigation .menu').addClass('mobile-menu');
+    }
+    jQuery(window).on('resize', function () {
+        if (jQuery('body').width() <= 1200) {
+            jQuery('#site-navigation .menu').addClass('mobile-menu');
+        }
+    });
+
+    // Hide sub menus on desktop nav on resize
+    jQuery(window).on('resize', function () {
+        if (jQuery('body').width() > 1200) {
+            jQuery('#site-navigation .sub-menu').removeAttr('style');
+            jQuery('#site-navigation .toggle-sub-menu-active').removeClass('toggle-sub-menu-active');
+            jQuery('#site-navigation .menu').removeClass('mobile-menu');
+        }
+    });
+
+    // Add shadow to fixed nav after scroll
+    jQuery(window).scroll(function () {
+        if (jQuery(window).scrollTop() > 0) {
+            jQuery('#site-navigation.fixed-nav').addClass('nav-shadow');
+            jQuery('#site-navigation.fixed-nav .sub-menu').addClass('nav-shadow-sub');
+            jQuery('#site-navigation.fixed-nav ~ .search-toggle-container').addClass('nav-shadow-sub');
+        } else {
+            jQuery('#site-navigation.fixed-nav').removeClass('nav-shadow');
+            jQuery('#site-navigation.fixed-nav .sub-menu').removeClass('nav-shadow-sub');
+            jQuery('#site-navigation.fixed-nav ~ .search-toggle-container').removeClass('nav-shadow-sub');
+        }
+    });
+
 });
 //Set timeout for flashes
 setTimeout("$('.flashes').fadeOut('slow')", 5000)
