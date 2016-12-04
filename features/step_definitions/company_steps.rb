e31037e17949f11b3690e1978256586ec21b7d31
thesuss <lullisuss@gmail.com>
2016-12-04 23:39:46 +0700

    refactors category list and prettifies views. Hides checkboxes from company-form. Updates tests

--- features/step_definitions/company_steps.rb
+++ features/step_definitions/company_steps.rb
@@ -8,3 +8,13 @@ And(/^I am the page for company number "([^"]*)"$/) do |company_number|
   company = Company.find_by_company_number(company_number)
   visit company_path company
 end
+
+Then(/^I should be on the edit company page for "([^"]*)"$/) do |company_number|
+  company = Company.find_by_company_number(company_number)
+  expect(current_path).to eq company_path(company)
+end
+
+When(/^I am on the edit company page for "([^"]*)"$/) do |company_number|
+  company = Company.find_by_company_number(company_number)
+  visit edit_company_path company
+end
