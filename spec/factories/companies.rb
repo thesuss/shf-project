138b0b7ae8c6c7582251c95fe6f4cc7f1edf1126
Susanna <thesuss@users.noreply.github.com>
2016-12-08 08:04:36 +0100

    Fixes the bug of company website url (#68)
    * fixes the bug of company website url
    
    * added instructions for entering url and tested for expected link on company page

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,14 +1,14 @@
 FactoryGirl.define do
   factory :company do
-    name "SomeCompany"
-    company_number "0000000000"
-    phone_number "123123123"
-    email "thiscompany@example.com"
-    street "123 1st Street"
-    post_code "00000"
-    city "Hundborg"
-    region "D"
-    website "www.example.com"
+    name 'SomeCompany'
+    company_number '0000000000'
+    phone_number '123123123'
+    email 'thiscompany@example.com'
+    street '123 1st Street'
+    post_code '00000'
+    city 'Hundborg'
+    region 'D'
+    website 'http://www.example.com'
 
     transient do
       num_categories 0
