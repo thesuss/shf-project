34bf3d1e96ba360d58fc537fcc42bd3d7d4acabe
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-12-07 23:15:38 -0800

    checking the user state: have an application? a fully instantiated company?; + methods to be congruent with "...?" style

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def has_membership_application?
+    membership_applications.size > 0 && (membership_applications.select { |app| app.persisted?}).count > 0
+  end
+
+  def has_company?
+    has_membership_application? && (membership_applications.select {| app | app.company && app.company.persisted?}).count > 0
+  end
+
+  def is_admin?
+    admin
+  end
+
+  def is_member?
+    is_member || admin
+  end
 end
