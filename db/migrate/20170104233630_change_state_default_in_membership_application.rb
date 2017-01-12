db0ede7b3f3b5c2571a86744dcf2ac320a2b5f8e
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2017-01-12 00:26:46 -0800

    change any existing 'Pending' states to 'under_review' during migration

--- db/migrate/20170104233630_change_state_default_in_membership_application.rb
+++ db/migrate/20170104233630_change_state_default_in_membership_application.rb
@@ -3,5 +3,9 @@ class ChangeStateDefaultInMembershipApplication < ActiveRecord::Migration[5.0]
 
     change_column_default :membership_applications, :state, from: 'Pending', to: 'under_review'
 
+    MembershipApplication.where(state: 'Pending').find_each do |pending_app|
+      pending_app.state = 'under_review'
+      pending_app.save
+    end
   end
 end
