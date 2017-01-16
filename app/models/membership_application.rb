6ef44860dd19b83f7d4cb0e89b5d68ddd3cfd893
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2017-01-15 22:04:31 -0800

    add instance var to track if it's marked as ready for review (used by the edit form checkbox); update state transitions

--- app/models/membership_application.rb
+++ app/models/membership_application.rb
@@ -22,41 +22,49 @@ class MembershipApplication < ApplicationRecord
 
   aasm :column => 'state' do
 
-    state :under_review, :initial => true
+    state :new, :initial => true
+    state :under_review
     state :waiting_for_applicant
+    state :ready_for_review
     state :accepted
     state :rejected
 
 
-    event :reject do
-      after do
-        reject_membership
-      end
-      transitions from: [:under_review, :accepted], to: :rejected
-    end
-
-    event :accept do
-      after do
-        accept_membership
-      end
-      # if a mistake was made and the application should have been accepted, can change it to accept
-      transitions from: [:under_review, :rejected], to: :accepted, guard: [:paid?, :not_a_member?]
+    event :start_review do
+      transitions from: :new, to: :under_review, guard: :not_a_member?
+      transitions from: :ready_for_review, to: :under_review
     end
 
     event :ask_applicant_for_info do
-      transitions from: [:under_review], to: :waiting_for_applicant, guard: :not_a_member?
+      transitions from: :under_review, to: :waiting_for_applicant
     end
 
     event :cancel_waiting_for_applicant do
-      transitions from: [:waiting_for_applicant], to: :under_review
+      transitions from: :waiting_for_applicant, to: :under_review
     end
 
-    event :applicant_updated_info do
-      transitions from: [:waiting_for_applicant], to: :under_review, guard: :not_a_member?
+    event :is_ready_for_review do
+      transitions from: :waiting_for_applicant, to: :ready_for_review
+    end
+
+    event :accept do
+      transitions from: [:under_review, :rejected], to: :accepted, guard: [:paid?, :not_a_member?], after: :accept_membership
+    end
+
+    event :reject do
+      transitions from: [:under_review, :accepted], to: :rejected, after: :reject_membership
     end
 
   end
 
+  # these are only used by the submisssion form and are not saved to the db
+  def marked_ready_for_review
+    @marked_ready_for_review ||= (ready_for_review? ? 1 : 0)
+  end
+
+  def marked_ready_for_review=(value)
+    @marked_ready_for_review = value
+  end
 
   def swedish_organisationsnummer
     errors.add(:company_number, "#{self.company_number} är inte ett svenskt organisationsnummer") unless Orgnummer.new(self.company_number).valid?
