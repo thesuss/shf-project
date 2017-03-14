a38a79305ad0e69fe57a4b1806fb117d93080012
Ashley Engelund <weedySeaDragon@users.noreply.github.com>
2017-03-14 01:04:55 -0700

    Helper methods: don't display empty field (#186)
    * specification for the field_or_default helper method
    * 'field_or_none' method = show '' if there's no value for the field.  (calls the field_or_default helper with default = ''); formatting
    * use default value if 'value' isBlank?   ; get rid of unnecessary comment in the spec

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,15 @@
 module ApplicationHelper
+
+
   def flash_class(level)
     case level.to_sym
-      when :notice then 'success'
-      when :alert then 'danger'
+      when :notice then
+        'success'
+      when :alert then
+        'danger'
     end
   end
+
 
   def flash_message(type, text)
     flash[type] ||= []
@@ -14,6 +19,7 @@ module ApplicationHelper
     flash[type] << text
   end
 
+
   def render_flash_message(flash_value)
     if flash_value.instance_of? String
       flash_value
@@ -22,9 +28,11 @@ module ApplicationHelper
     end
   end
 
+
   def translate_and_join(error_list)
-    error_list.map{|e| I18n.t(e)}.join(', ')
+    error_list.map { |e| I18n.t(e) }.join(', ')
   end
+
 
   # ActiveRecord::Assocations::CollectionAssociation is a proxy and won't
   # always load info. see the class documentation for more info
@@ -35,7 +43,56 @@ module ApplicationHelper
 
 
   def i18n_time_ago_in_words(past_time)
-    "#{t('time_ago', amount_of_time: time_ago_in_words(past_time) )}"
+    "#{t('time_ago', amount_of_time: time_ago_in_words(past_time))}"
+  end
+
+
+  # call field_or_default with the default value = an empty String
+  def field_or_none(label, value, tag: :p, tag_options: {}, separator: ': ',
+                    label_class: 'field-label', value_class: 'field-value')
+
+    field_or_default(label, value, default: '', tag: tag, tag_options: tag_options, separator: separator,
+                     label_class: label_class, value_class: value_class)
+  end
+
+
+  # Return the HTML for a simple field with "Label: Value"
+  # If value is blank, return the value of default (default value for default = '')
+  # Surround it with the given content tag (default = :p if none provided)
+  # and use the tag options (if any provided).
+  # Default class to surround the label and separator is 'field-label'
+  # Default class to surround the value is 'field-value'
+  #
+  # Separate the Label and Value with the separator string (default = ': ')
+  #
+  #  Ex:  field_or_none('Name', 'Bob Ross')
+  #     will produce:  "<p><span class='field-label'>Name: </span><span class='field-value'>Bob Ross</span></p>"
+  #
+  #  Ex:  field_or_default('Name', '', default: '(no name provided)')
+  #     will produce:  "(no name provided)"
+  #
+  #  Ex:  field_or_default('Name', '', default: content_tag( :h4, '(no name provided)', class: 'empty-warning') )
+  #     will produce:  "<h4 class='empty-warning'>(no name provided)</h4>"
+  #
+  # Ex: field_or_none('Name', 'Bob Ross', tag: :h2, separator: ' = ')
+  #     will produce:  "<h2><span class='field-label'>Name = </span><span class='field-value'>Bob Ross</span></h2>"
+  #
+  # Ex: field_or_none('Name', 'Bob Ross', tag_options: {id: 'bob-ross'}, value_class: 'special-value')
+  #     will produce:  "<p id='bob-ross'><span class='field-label'>Name: </span><span class='special-value'>Bob Ross</span></p>"
+  #
+  def field_or_default(label, value, default: '', tag: :p, tag_options: {}, separator: ': ',
+                       label_class: 'field-label', value_class: 'field-value')
+
+
+    if value.blank?
+      default
+    else
+      content_tag(tag, tag_options) do
+        concat content_tag(:span, "#{label}#{separator}", class: label_class)
+        concat content_tag(:span, value, class: value_class)
+      end
+    end
+
   end
 
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe ApplicationHelper, type: :helper do
 
     describe 'adds message to a flash[type] that already has messages' do
 
-      before (:each) do
+      before(:each) do
         helper.flash_message @flash_type, @second_message
       end
 
@@ -71,5 +71,69 @@ RSpec.describe ApplicationHelper, type: :helper do
     t = Time.now - 1.day
     expect(helper.i18n_time_ago_in_words(t)).to eq("#{I18n.t('time_ago', amount_of_time: time_ago_in_words(t))}")
   end
+
+
+
+  #
+  # Separate the Label and Value with the separator string (default = ': ')
+  #
+  #  Ex:  field_or_default('Name', 'Bob Ross')
+  #     will produce:  "<p><span class='field-label'>Name: </span><span class='field-value'>Bob Ross</span></p>"
+  #
+  # Ex: field_or_default('Name', 'Bob Ross', tag: :h2, separator: ' = ')
+  #     will produce:  "<h2><span class='field-label'>Name = </span><span class='field-value'>Bob Ross</span></h2>"
+  #
+  # Ex: field_or_default('Name', 'Bob Ross', tag_options: {id: 'bob-ross'}, value_class: 'special-value')
+  #     will produce:  "<p id='bob-ross'><span class='field-label'>Name: </span><span class='special-value'>Bob Ross</span></p>"
+
+  describe '#field_or_default' do
+
+    it 'nil value returns an empty string' do
+      expect(helper.field_or_default('some label', nil)).to eq ''
+    end
+
+    it 'empty value returns an empty string by default' do
+      expect(helper.field_or_default('some label', '')).to eq ''
+    end
+
+
+    it "can set the default string to a complicated content_tag " do
+    #  expect(helper.field_or_default('some label', [], default: (content_tag(:div, class: ["strong", "highlight"]) { 'some default' }) )).to eq('some default')
+    end
+
+
+    it 'non-empty value with defaults == <p><span class="field-label">labelseparator</span><span class="field-value">value</span></p>' do
+      expect(helper.field_or_default('label', 'value')).to eq('<p><span class="field-label">label: </span><span class="field-value">value</span></p>')
+    end
+
+
+    it 'can set a custom separator' do
+      expect(helper.field_or_default('label', 'value', separator: '???')).to eq('<p><span class="field-label">label???</span><span class="field-value">value</span></p>')
+    end
+
+
+    it 'can set the class of the surrounding tag' do
+      expect(helper.field_or_default('label', 'value', tag: :h2)).to eq('<h2><span class="field-label">label: </span><span class="field-value">value</span></h2>')
+    end
+
+
+    it 'can set html options for the surrounding tag' do
+      expect(helper.field_or_default('label', 'value', tag_options: {class: "blorf", id: "blorfid"})).to eq('<p class="blorf" id="blorfid"><span class="field-label">label: </span><span class="field-value">value</span></p>')
+    end
+
+
+    it "can set class for the label + separator = 'special-label-class'" do
+      expect(helper.field_or_default('label', 'value', label_class: 'special-label-class')).to eq('<p><span class="special-label-class">label: </span><span class="field-value">value</span></p>')
+    end
+
+
+    it "default class for the value = 'special-value-class'" do
+      expect(helper.field_or_default('label', 'value', value_class: 'special-value-class')).to eq('<p><span class="field-label">label: </span><span class="special-value-class">value</span></p>')
+    end
+
+  end
+
+
+
 
 end
