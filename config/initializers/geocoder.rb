b5eacdd221ed23be8047beadadd0dd1b92ae39ab
Ashley Engelund <weedySeaDragon@users.noreply.github.com>
2017-04-18 14:01:20 -0700

    Increase Geocoder timeout; dot-env gem; read Google API from ENV (#219)
    * use dot-env gem in all Rails environments
    
    (cherry picked from commit bf8af77)
    
    * increase Geocoder timeout (length of time to wait); show all errors in development & test enviornments
    
    (cherry picked from commit f7d0e60)
    
    * get GoogleAPI key from ENV; hardcode URL to the google map api js
    
    (cherry picked from commit 089fe8f)
    
    * increase Gecoder timeout to 9 seconds (failed on Semaphore when == 5 seconds)
    
    (cherry picked from commit 3edf75b)
    
    * forcing a new Semaphore build on Github (prev. build failed due to Google maps API limit.) whitespace formatting

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,9 +1,9 @@
 Geocoder.configure(
     # Geocoding options
-    # timeout: 3,                 # geocoding service timeout (secs)
+     timeout: 9,                  # geocoding service timeout (secs)
     # lookup: :google,            # name of geocoding service (symbol)
 
-    language: :sv,              # ISO-639 language code
+    language: :sv,                # ISO-639 language code
 
     # use_https: false,           # use HTTPS for lookup requests? (if supported)
     # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
@@ -20,8 +20,11 @@ Geocoder.configure(
     # supports SocketError and Timeout::Error
     # always_raise: [],
 
+
     # Calculation options
     units: :km,                 # :km for kilometers or :mi for miles
 
     # distances: :linear          # :spherical or :linear
 )
+
+Geocoder.configure(always_raise: :all) unless Rails.env.production?
