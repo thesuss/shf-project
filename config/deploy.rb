b07dc63054f136a05b5faef76bb8e34a2d3c58e2
Ashley Engelund <weedySeaDragon@users.noreply.github.com>
2017-09-28 10:00:10 -0700

    share app/views/pages across all releases (capistrano deploy configuration) (#364)

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,6 +31,7 @@ set :linked_dirs, %w{
   public/.well-known
   public/storage
   public/ckeditor_assets
+  app/views/pages
 }
 
 # public/.well-known  created by diffouo (raoul) when this was set up. used for ??? (not used?)
@@ -38,6 +39,7 @@ set :linked_dirs, %w{
 # public/uploads      created by diffouo (raoul) when this was set up. used for ??? (not used?)
 # public/storage  Files uploaded for  membership applications
 # public/ckeditor_assets Files uploaded by members, admins when using the ckeditor (ex: company page custom infor, SHF member documents)
+# app/views/pages  Member Documents are stored here.  (Eventually they should moved to a different directory)
 
 
 set :keep_releases, 5
