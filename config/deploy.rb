2cf5651d11420bf2d4aa323634ec495c03c6ec58
Ashley Engelund <weedySeaDragon@users.noreply.github.com>
2017-11-27 11:33:42 -0800

    Add capistrano tasks rails:console and rails:dbconsole (#400)
    * bin should not be shared; ensure bin is written fresh for every deploy so it's up to date
    
    * capistrano tasks to connect to rails console, dbconsole on the server

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :linked_files, %w{config/database.yml config/secrets.yml .env}
 # These directories are in the 'shared' directory on the production system: /var/www/shf/shared/
 # (That is the convention for Capistrano deployments.)
 set :linked_dirs, %w{
-  bin log tmp/pids tmp/cache tmp/sockets vendor/bundle
+  log tmp/pids tmp/cache tmp/sockets vendor/bundle
   public/system
   public/uploads
   public/.well-known
@@ -41,6 +41,8 @@ set :linked_dirs, %w{
 # public/ckeditor_assets Files uploaded by members, admins when using the ckeditor (ex: company page custom infor, SHF member documents)
 # app/views/pages  Member Documents are stored here.  (Eventually they should moved to a different directory)
 
+# ensure the binstubs (files in /bin) are generated
+set :bundle_binstubs, nil
 
 set :keep_releases, 5
 
@@ -54,4 +56,43 @@ namespace :deploy do
   end
 
   after :publishing, :restart
+end
+
+
+
+# Run a rails console or a rails dbconsole
+# @url https://gist.github.com/toobulkeh/8214198
+#
+# Note: this assumes that there is a /bin/rails  file that is capable of
+# running a rails console.
+#
+# Usage:
+#  bundle exec cap production rails:console
+#  bundle exec cap production rails:dbconsole
+namespace :rails do
+  desc "Open the rails console"
+  task :console do
+    on roles(:app) do
+      rails_env = fetch(:rails_env, 'production')
+      execute_interactively "$HOME/.rbenv/bin/rbenv exec bundle exec rails console #{rails_env}"
+    end
+  end
+
+  desc "Open the rails dbconsole"
+  task :dbconsole do
+    on roles(:app) do
+      rails_env = fetch(:rails_env, 'production')
+      execute_interactively "$HOME/.rbenv/bin/rbenv exec bundle exec rails dbconsole #{rails_env}"
+    end
+  end
+
+
+  # ssh to the server
+  def execute_interactively(command)
+    server = fetch(:bundle_servers).first
+    user = server.user
+    port = server.port || 22
+
+    exec "ssh -l #{user} #{host} -p #{port} -t 'cd #{deploy_to}/current && #{command}'"
+  end
 end
