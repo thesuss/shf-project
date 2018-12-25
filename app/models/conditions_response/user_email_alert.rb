7aed11a6a5e0e31c6b6c3e9bfc8b9c8297e0fd75
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2018-12-25 01:45:54 -0800

    abstract shared behavior to parent class (EmailAlert)

--- app/models/conditions_response/user_email_alert.rb
+++ app/models/conditions_response/user_email_alert.rb
@@ -1,106 +1,48 @@
 # Abstract parent class for all alerts that send out emails to users
 #
-#  * knows if an alert needs to be sent today (or on _this_date_)
-#  * knows the name of the method to send to MemberMailer to send out the
-#     alert to a user
+# Responsibility: sets the information needed so that the parent class(es) can
+#  process the condition for an email sent to Users
 #
-#  All dates used are a Date (not a Time or DateTime). This allows us to easily
-#  determine the number of days between two dates.
+#  SUBCLASSES MUST REDEFINE THESE METHODS:
+#     send_alert_this_day?
+#     mailer_method
 #
-class UserEmailAlert < ConditionResponder
+class UserEmailAlert < EmailAlert
 
 
-  def self.condition_response(condition, log)
-
-    config = get_config(condition)
-    timing = get_timing(condition)
-
-    User.all.each do |user|
-
-      if send_alert_this_day?(timing, config, user)
-        begin
-          mail_response = MemberMailer.send(mailer_method, user).deliver_now
-          log_mail_response(log, mail_response, user.id, user.email)
-
-        rescue => mailing_error
-          log_failure(log, log_msg_start,
-                      user_info(user.id, user.email),
-                      mailing_error)
-        end
-      end
-
-    end
-
+  def entities_to_check
+    User.all
   end
 
 
-  # method to improve readability. returns true if day_number is in config[:days]
+  def mailer_class
+    MemberMailer
+  end
+
+
+  def mailer_args(user)
+    [user]
+  end
+
+
+  def success_str(user)
+    user_info(user)
+  end
+
+
+  # Return a log string maker specific to this class and subclasses
   #
-  # Expects config to include the :days key; returns false if it is not there
-  #
-  # If config is not a Hash, raises an Error because it really _should_ be a Hash
-  #  (it's a programming error to call this otherwise!)
-  #
-  # @param day_number [Integer] - the number of days away from today (before, after, or on)
-  # @param config [Hash] - other configuration info
-  def self.send_on_day_number?(day_number, config)
-    config.fetch(:days, false) ? config[:days].include?(day_number) : false
+  def log_str_maker
+    @@log_str_maker ||= AlertLogStrMaker.new(self, :success_str, :failure_str)
   end
 
 
-  # Subclasses should define this by checking information for the user
-  # computing whatever date information is necessary to determine if
-  # an alert should be sent out today.
-  # Ex:
-  #   def self.send_alert_this_day?(config, user)
-  #     days_until = (user.membership_expire_date - Date.current).to_i
-  #     user.membership_current? &&  config[:days].include?(days_until)
-  #   end
-  #
-  # @param timing [Timing] - the relative timing of the alerts
-  # @param config [Object] - configuration information used to determine if an alert should be sent
-  # @param user [User] - the user we are checking and, if appropriate, will send the alert to
-  #
-  def self.send_alert_this_day?(_timing, _config, _user)
-    raise NoMethodError, "Subclass must define the #{__method__} method and return true or false", caller
+  def failure_str(user)
+    user_info(user)
   end
 
 
-  # This is the method sent to the MemberMailer when this condition
-  # needs to send out an email.
-  #
-  # Subclasses must redefine this and return a symbol
-  # Ex:
-  #   def self.mailer_method
-  #     :membership_expiration_reminder
-  #   end
-  def self.mailer_method
-    raise NoMethodError, "Subclass must define the #{__method__} method and return a Symbol", caller
-  end
-
-
-  # This string is the start of the line logged to the Alert log when
-  # this condition sends out an email = the name of the class
-  def self.log_msg_start
-    self.name
-  end
-
-
-  def self.log_mail_response(log, mail_response, user_id, user_email)
-    user_info_str = user_info(user_id, user_email)
-    mail_response.errors.empty? ? log_success(log, log_msg_start, user_info_str)
-        : log_failure(log, log_msg_start, user_info_str)
-  end
-
-  def self.log_success(log, msg_start, user_info_str)
-    log.record('info', "#{msg_start} email sent #{user_info_str}.")
-  end
-
-  def self.log_failure(log, msg_start, user_info_str, error = '')
-    log.record('error', "#{msg_start} email ATTEMPT FAILED #{user_info_str}. #{error} Also see for possible info #{ApplicationMailer::LOG_FILE} ")
-  end
-
-  def self.user_info(id, email)
-    "to id: #{id} email: #{email}"
+  def user_info(user)
+    user.nil? ? "user is nil" : "to id: #{user.id} email: #{user.email}"
   end
 end
