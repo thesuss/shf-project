15ac25268d398802695130abe55fb6a87aa69b0a
Ashley Engelund <weedySeaDragon@users.noreply.github.com>
2019-03-07 09:51:54 -0800

    Membership Lapsed alert (#622)
    * add MembershipLapsed to load_conditions
    
    * sv locale: pay_on_acct_page = Du betalar via; en users:your_account to be same as sv
    
    * optional argument to capitalize "my account" link text
    
    * fix text for "pay on my account page"

--- app/helpers/member_mailer_helper.rb
+++ app/helpers/member_mailer_helper.rb
@@ -3,8 +3,10 @@ module MemberMailerHelper
   # The link and text to a member's account page (to be used in emails).
   # Uses _blank so that it will open in a new browser window.
   #
-  def member_account_link(member)
-    link_to(t('menus.nav.users.your_account'), user_url(member), target: '_blank', rel: 'nofollow')
+  def member_account_link(member, capitalize: false)
+    link_text = t('menus.nav.users.your_account')
+    link_text = capitalize ? link_text.capitalize : link_text.downcase
+    link_to(link_text, user_url(member), target: '_blank', rel: 'nofollow')
   end
 
 end
